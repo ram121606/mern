2da6c5b71d686ea56c286de9f3ecd36060cc9562
ram121606 <ramasubramanyam12@gmail.com>
2024-01-06 09:23:47 +0530

    added forms.html

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+let val = prompt("Enter the color");
+switch(val){
+    case 'violet':
+        alert("Violet")
+        break
+    case 'indigo':
+        alert("Indigo")
+        break
+    case 'blue':
+        alert("Blue")
+        break
+    case 'green':
+        alert("green")
+        break
+    case 'yellow':
+        alert("Yellow")
+        break
+    case 'orange':
+        alert("Orange")
+        break
+    case 'red':
+        alert("Red")
+        break
+    default:
+        alert("Not a rainbow color")
+}
